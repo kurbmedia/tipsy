fefd95b1b3c98e732d2d8846dc6d743492884b5e
Brent Kirby <brent@kurbmedia.com>
2011-08-20 03:29:43 -0400

    adding view contexts

--- lib/tipsy/view/base.rb
+++ lib/tipsy/view/base.rb
@@ -1,6 +1,6 @@
-require 'tipsy/view/errors'
 require 'hike'
 require 'tilt'
+require 'tipsy/view/errors'
 
 module Tipsy
   class View
@@ -14,7 +14,23 @@ module Tipsy
     def view_context
       @_view_context ||= Class.new(self)
         include Tipsy::Helpers
+      end.class_eval(_register_helpers)
+    end
+    
+    private
+    
+    def template
+      @_template ||= 
+    end
+    
+    def _register_helpers
+      module_names = []
+      includes = Dir.glob(File.expand_path(Tipsy.root) << "/helpers/*.rb").inject([]) do |arr, helper|
+        module_names << File.basename(helper, '.rb').classify
+        arr.concat File.open(helper).readlines          
       end
+      module_names.each{ |mod| includes.concat(["\n include #{mod}"]) }
+      includes.join("\n")
     end
     
   end
